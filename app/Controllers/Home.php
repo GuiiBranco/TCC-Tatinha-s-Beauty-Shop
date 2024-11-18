1f20e253981949da4092bf1d020844d02a86598c
Guilherme Branco Rodrigues <guidriguesbrnc@gmail.com>
2024-11-17 23:00:54 -0300

    Backup de precaução

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -2,10 +2,27 @@
 
 namespace App\Controllers;
 
+use App\Models\PortfolioModel;
+
 class Home extends BaseController
 {
     public function index(): string
     {
-        return view('index');
+        $dados = array();
+        $portfolioModel = new PortfolioModel();
+        $itens = $portfolioModel->findAll();
+        $dados["imagens"] = $itens;
+
+        return view('index', $dados);
+    }
+
+    public function teste()
+    {
+        $dados = array();
+        $portfolioModel = new PortfolioModel();
+        $itens = $portfolioModel->findAll();
+        $dados["imagens"] = $itens;
+
+        var_dump($dados["imagens"]);
     }
 }
